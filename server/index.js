6350885a510c3e3bbef476a9fa7406616117704e
Mykyta Prus <mike.prus@ekreative.com>
2022-09-19 16:25:49 +0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const http = require("http").Server(app);
+const PORT = 4000;
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3001",
+  },
+});
+
+app.use(cors());
+
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  socket.on("message", (data) => {
+    socketIO.emit("messageResponse", data);
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello" });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
